c6ff378dde6b1ec205638992e494f607c9219d58
DESKTOP-FD4CQOG\Gizmo <andrea_v@post.cz>
2023-04-23 15:16:59 +0200

    Domaci ukol: Lekce 1

--- test/specs/homework/homework.e2e.js
+++ test/specs/homework/homework.e2e.js
@@ -2,8 +2,12 @@ describe('Homework', async () => {
 
     it('should open page and create screenshot', async () => {
 
-        // sem vypracuj domácí úkol
+        await browser.reloadSession();
 
+        await browser.url('/registrace');
+        
+        await browser.saveScreenshot('screenshot.png');
+        
     });
 
 });
