8f3c9147ba3c2bd7f79163e45cb3fd1a3d8f885a
DESKTOP-FD4CQOG\Gizmo <andrea_v@post.cz>
2023-05-02 18:55:48 +0200

    2.domaci ukol

--- test/specs/exercise.e2e.js
+++ test/specs/exercise.e2e.js
@@ -10,7 +10,62 @@ describe('Czechitas Login Page', async () => {
 
         await browser.url('/prihlaseni');
 
-        await browser.pause(5000);
+        const emailField = await $('#email');
+        console.log(await emailField.getHTML());
+        
+
+        //1. podle tagu
+        const fieldPodleTagu1 = await $('form');
+        console.log(await fieldPodleTagu1.getHTML());
+        const fieldPodleTagu2 = await $('input');
+        console.log(await fieldPodleTagu2.getHTML());
+        const fieldPodleTagu3 = await $('button');
+        console.log(await fieldPodleTagu3.getHTML());
+
+        //2. podle ID
+        const fieldPodleID1 = await $('#email');
+        console.log(await fieldPodleID1.getHTML());
+        const fieldPodleID2 = await $('#password');
+        console.log(await fieldPodleID2.getHTML());
+
+        //3. podle třídy
+        const fieldPodleTridy1 = await $('.btn-primary');
+        console.log(await fieldPodleTridy1.getHTML());
+        const fieldPodleTridy2 = await $('[name="email"]');
+        console.log(await fieldPodleTridy2.getHTML());
+
+        //4. podle atributu
+        const fieldPodleAtributu1 = await $('[type="password"]');
+        console.log(await fieldPodleAtributu1.getHTML());
+        const fieldPodleAtributu2 = await $('[type*="ass"]');
+        console.log(await fieldPodleAtributu2.getHTML());
+        const fieldPodleAtributu3 = await $('[type$="word"]');
+        console.log(await fieldPodleAtributu3.getHTML());
+        const fieldPodleAtributu4 = await $('[type^="pass"]');
+        console.log(await fieldPodleAtributu4.getHTML());
+
+        //5. kombinace
+        const fieldPodleKombinace1 = await $('input#email');
+        console.log(await fieldPodleKombinace1.getHTML());
+        const fieldPodleKombinace2 = await $('input[type="password"]');
+        console.log(await fieldPodleKombinace2.getHTML());
+        const fieldPodleKombinace3 = await $('button.btn-primary');
+        console.log(await fieldPodleKombinace3.getHTML());
+
+        //6. řetězení selektorů
+        const fieldPodleRetezeniSelektoru = await $('div').$('form').$('input[type$="word"]');
+        console.log(await fieldPodleRetezeniSelektoru.getHTML());
+
+        //7.WDIO selektory
+        const fieldPodleWDIO = await $('=Zapomněli jste své heslo?');
+        console.log(await fieldPodleWDIO.getHTML());
+
+
+
+
+
+
+        await browser.pause(500);
 
     });
 
--- test/specs/homework/homework.e2e.js
+++ test/specs/homework/homework.e2e.js
@@ -7,6 +7,27 @@ describe('Homework', async () => {
         await browser.url('/registrace');
         
         await browser.saveScreenshot('screenshot.png');
+
+
+        //selektor pro jméno
+        const nameField = await $('#name');
+        console.log(await nameField.getHTML());
+
+        //selektor pro email
+        const emailField = await $('#email');
+        console.log(await emailField.getHTML());
+
+        //selektor pro heslo
+        const passwordField = await $('#password');
+        console.log(await passwordField.getHTML());
+
+        //selektor pro heslo
+        const passwordConfirmField = await $('#password-confirm');
+        console.log(await passwordConfirmField.getHTML());
+
+        //selektor pro tlačítko registrace
+        const btnPrimaryField = await $('.btn-primary');
+        console.log(await btnPrimaryField.getHTML());
         
     });
 
